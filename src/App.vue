6f7f6305b0c5504b60943d3dcd270c615e66a211
luizlivio <livio44@gmail.com>
2021-05-28 20:21:30 -0300

    Criação dos componentes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Navbar/>
+  <TopBanner/>
+  <AboutMe/>
+  <Recommendations/>
+  <Skills/>
+  <Promo/>
+  <Products/>
+  <Testimonials/>
+  <Contact/>
+  <Footer/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AboutMe from './components/AboutMe.vue'
+import Contact from './components/Contact.vue'
+import Footer from './components/Footer.vue'
+import Navbar from './components/Navbar.vue'
+import Products from './components/Products.vue'
+import Promo from './components/Promo.vue'
+import Recommendations from './components/Recommendations.vue'
+import Skills from './components/Skills.vue'
+import Testimonials from './components/Testimonials.vue'
+import TopBanner from './components/TopBanner.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    AboutMe,
+    Contact,
+    Footer,
+    Navbar,
+    Products,
+    Promo,
+    Recommendations,
+    Skills,
+    Testimonials,
+    TopBanner
   }
 }
 </script>
---
+++ src/components/TopBanner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TopBanner"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
